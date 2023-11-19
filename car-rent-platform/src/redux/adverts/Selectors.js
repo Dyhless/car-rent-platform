531d91ac4b31550b8e6fd162ad264d837d601eca
Dmytro Medvediev <dsisitkin@gmail.com>
2023-11-19 23:37:45 +0100

    update 404

--- car-rent-platform/src/redux/adverts/Selectors.js
+++ car-rent-platform/src/redux/adverts/Selectors.js
@@ -1,20 +1,20 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getFavorites = (state) => state.favorites;
 
-// import { createSelector } from '@reduxjs/toolkit';
+export const selectCars = state => state.adverts.items;
+export const selectFilter = state => state.filter;
+export const selectAmountCars = state => state.adverts.items.length;
 
-// export const selectCars = state => state.adverts.items;
-// export const selectFilter = state => state.filter;
-// export const selectAmountCars = state => state.adverts.items.length;
-
-// export const selectFilteredCars = createSelector(
-//   [selectCars, selectFilter],
-//   (adverts, { make, mileageFrom, mileageTo, rentalPrice }) => {
-//     return adverts
-//       .filter((advert) => advert.make && advert.make.toLowerCase().includes(make.toLowerCase()))
-//       .filter((advert) => (rentalPrice > 0 ? Number(advert.rentalPrice.split('$')[1]) === rentalPrice : true))
-//       .filter((advert) => (mileageFrom > 0 ? advert.mileage >= mileageFrom : true))
-//       .filter((advert) => (mileageFrom > 0 && mileageTo > 0 ? advert.mileage <= mileageTo : true))
-//       .filter((advert) => (mileageFrom === 0 && mileageTo > 0 ? advert.mileage <= mileageTo : true));
-//   }
-// );
+export const selectFilteredCars = createSelector(
+  [selectCars, selectFilter],
+  (adverts, { make, mileageFrom, mileageTo, rentalPrice }) => {
+    return adverts
+      .filter((advert) => advert.make && advert.make.toLowerCase().includes(make.toLowerCase()))
+      .filter((advert) => (rentalPrice > 0 ? Number(advert.rentalPrice.split('$')[1]) === rentalPrice : true))
+      .filter((advert) => (mileageFrom > 0 ? advert.mileage >= mileageFrom : true))
+      .filter((advert) => (mileageFrom > 0 && mileageTo > 0 ? advert.mileage <= mileageTo : true))
+      .filter((advert) => (mileageFrom === 0 && mileageTo > 0 ? advert.mileage <= mileageTo : true));
+  }
+);
 
